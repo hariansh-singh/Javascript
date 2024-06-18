0160706216a4ad86e950db842c6ff65c3840bd4f
hariansh-singh <harianshsingh13@gmail.com>
2024-06-18 10:40:18 +0530

    refactor: Remove unused code and optimize array operations in 05_date.js

--- js/05_date.js
+++ js/05_date.js
@@ -15,3 +15,18 @@ const arr2 = new Array(3, 4, 18)
 console.log(arr2)
 
 arr2.push(17)
+console.log(arr2);
+
+arr2.pop()
+console.log(arr2);
+
+arr2.unshift(9)
+console.log(arr2);
+
+arr2.shift()
+console.log(arr2);
+
+const newArr = arr2.join()
+
+console.log(newArr);
+console.log(typeof newArr);
